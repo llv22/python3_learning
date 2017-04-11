098cf3d74f8b5a8546cbac8777767f32d46d121e
Orlando Ding <xiandao.airs@gmail.com>
2017-04-11 22:06:54 +0800

    llv23 - for updates of sample.by for eliminating error of func pointer

--- chapter4/sample.py
+++ chapter4/sample.py
@@ -3,9 +3,11 @@ Learning python3
 """
 def document_it(func):
     '''
+    decractor for func, only print doc of func.
     '''
     def new_function(*args, **kwargs):
         '''
+        internal function for wrappering of func and print out function parameter and result.
         '''
         print('Running functions:', func.__name__)
         print('Positional arguments:', args)
@@ -16,16 +18,19 @@ def document_it(func):
     return new_function
 
 @document_it
-def add_ints0(a, b):
+def add_ints0(add_a, add_b):
     '''
+    add with decrator of @document_it.
     '''
-    return a + b
+    return add_a + add_b
 
 def square_it(func):
     '''
+    decractor for func, return square of func returned value.
     '''
     def new_function(*args, **kwargs):
         '''
+        internal function for wrappering of func and return square of func as result.
         '''
         result = func(*args, **kwargs)
         return result * result
@@ -33,15 +38,18 @@ def square_it(func):
 
 @document_it
 @square_it
-def add_ints1(a, b):
+def add_ints1(add_a, add_b):
     '''
+    add with decrator of @square_it @document_it in order.
     '''
-    return a + b
+    return add_a + add_b
 
 
 @square_it
 @document_it
-def add_ints2(a, b):
+def add_ints2(add_a, add_b):
     '''
+    add with decrator of @document_it @square_it in order.
     '''
-    return a + b
+    return add_a + add_b
+    
